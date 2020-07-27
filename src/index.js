728e314e172c32b7e26d72f40556f3a91b645ab5
zhangwenjie16 <zhangwenjie16@meituan.com>
2020-07-27 20:38:37 +0800

    update: show test data by props

--- src/index.js
+++ src/index.js
@@ -1,41 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import moment from 'moment';
 
-function Tweet() {
+function Tweet({ tweet }) {
   return (
     <div className="tweet">
-      <Avatar /> 
+      <Avatar hash={tweet.gravatar}/>
       <div className="content">
-        <NameWithHandle/>
-        <Message/>
+        <NameWithHandle author={tweet.author}/><Time time={tweet.timestamp}/>
+        <Message text={tweet.message}/>
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
       </div>
     </div>
   );
 }
 
-function Avatar(){
+function Avatar({hash}) {
   return <img
-   src="https://cdn.pixabay.com/photo/2020/07/14/15/43/church-5404699_1280.jpg"
-   className="avatar"
-   alt="avatar" />;
+    src={hash}
+    className="avatar"
+    alt="avatar" />;
 }
 
-function Message(){
+function Message({text}) {
   return (
     <div className="message">
-      This is less than 140 characters.
+      {text}
     </div>
   );
 }
 
-function NameWithHandle(){
+function NameWithHandle({author}) {
+  const {name, handle} = author || {};
   return (
     <span className="name-with-handle">
-      <span className="name">Your name</span>
-      <span className="handle">@yourhandle</span>
+      <span className="name">{name}</span>
+      <span className="handle">@{handle}</span>
     </span>
   );
 }
 
-ReactDOM.render(<Tweet />, document.querySelector('#root'));
+const Time = ({time}) => {
+  const timeString = moment(time).fromNow();
+  return<span className="time">{timeString}</span>;
+}
+
+const ReplyButton = () => (
+  <i className="fa fa-reply reply-button" />
+);
+
+const RetweetButton = () => (
+  <i className="fa fa-retweet retweet-button" />
+);
+
+const LikeButton = () => (
+  <i className="fa fa-heart like-button" />
+);
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+var testTweet = {
+  message: "Something about cats.",
+  gravatar: "https://cdn.pixabay.com/photo/2020/07/14/15/43/church-5404699_1280.jpg",
+  author: {
+    handle: "catperson",
+    name: "IAMA Cat Person"
+  },
+  likes: 2,
+  retweets: 0,
+  timestamp: "2016-07-30 21:24:37"
+}
+
+ReactDOM.render(<Tweet tweet={testTweet}/>, document.querySelector('#root'));
